5eb06449579531ec1b5d7e316939d56bcf69fcf3
Lisa <biange0923@gmail.com>
2026-06-21 11:56:42 +0200

    Add create_database.py

---
+++ sql/create_database.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import sqlite3
+
+df_alls = pd.read_parquet("data/processed/df_alls_cleaned.parquet")
+
+conn = sqlite3.connect("electricity.db")
+
+df_alls["year"] = df_alls.index.year
+df_alls["month"] = df_alls.index.month
+df_alls["hour"] = df_alls.index.hour
+
+df_alls.to_sql(
+    "electricity",
+    conn,
+    if_exists= "replace",
+    index = False
+)
+
+cursor = conn.cursor()
+
+cursor.execute(
+    "SELECT COUNT(*) FROM electricity"
+)
+
+print(cursor.fetchone())
+
+conn.close()
+
+print("Database created successfully.")
